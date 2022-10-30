adc7ec2c154e8638c0fb0a20831cc77be2c24640
agustinchoslr <agustinchoslr@gmail.com>
2022-10-30 14:14:46 -0300

    corrigiendo cors en el backend

--- src/main/java/tech/agustinchoslr/portfolio/PortfolioApplication.java
+++ src/main/java/tech/agustinchoslr/portfolio/PortfolioApplication.java
@@ -21,7 +21,7 @@ public class PortfolioApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://argprog-24b53.web.app"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://argprog-24b53.web.app/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
